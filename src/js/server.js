f27a840fe964183fbbba0524861d4872a1a5814f
MaximGromak <maximgromak@gmail.com>
2022-01-29 18:01:51 +1000

    initial commit

---
+++ src/js/server.js
@@ -0,0 +1,40 @@
+const WebSocketServer = require('ws');
+
+PORT = 9999;
+
+const wss = new WebSocketServer.Server({port: PORT});
+
+wss.on('connection', onConnect);
+
+function onConnect(wsClient) {
+    console.log('Новый пользователь');
+    wsClient.send('Привет');
+
+    wsClient.on('close', function () {
+        console.log('Пользователь отключился');
+    });
+
+    wsClient.on('message', function (message) {
+        console.log(message);
+        try {
+            const jsonMessage = JSON.parse(message);
+            switch (jsonMessage.action) {
+                case 'ECHO':
+                    wsClient.send(jsonMessage.data);
+                    break;
+                case 'PING':
+                    setTimeout(function () {
+                        wsClient.send('PONG');
+                    }, 2000);
+                    break;
+                default:
+                    console.log('Неизвестная команда');
+                    break;
+            }
+        } catch (error) {
+            console.log('Ошибка', error);
+        }
+    });
+}
+
+console.log('Сервер запущен на ' + PORT + ' порту');
